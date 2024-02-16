1fc319c1c56435ca664a74082ac589bdb3c52a1b
Nikhil Gupta <nguptaa183@gmail.com>
2024-02-15 20:39:47 -0500

    Add Files

--- try.py
+++ try.py
@@ -1,19 +1,9 @@
-# 9, 8, 7, 6, 5, 4, 8, 6, 5, 3, 2 ,1
-dum = float('inf')
-max_con = 0
-cnt = 0
+def func():
+    hello = 45
+    stt = "err"
+    print("Hello Words" + stt)
+    return hello
 
-while True:
-
-    inp = int(input())
-    if inp < dum:
-        dum = inp  # 9, 8,7,6,5,4
-        cnt = 0 # 
-    else:
-        cnt += 1
-        max_con = max(max_con, cnt)
-        if max_con == 3:
-            print("dum= ",dum)
-            break
-
-print("Max_con=  ",max_con)
+# Call the function
+result = func()
+print("Returned Value:", result)
